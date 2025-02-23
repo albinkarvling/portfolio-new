7a533178e1220db3c359030eead1239a138971c1
Albin Kärvling <albin.karvling@atg.se>
2025-02-23 22:29:38 +0100

    setup meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,32 @@ const montserrat = Montserrat({
     subsets: ["latin"],
 });
 
-export const metadata: Metadata = {};
+const url = "https://albinkarvling.com";
+const name = "Albin Kärvling";
+const title = name;
+const description =
+    "A personal portfolio showcasing my projects, skills, and experience as a fullstack developer, who specializes in frontend development.";
+
+export const metadata: Metadata = {
+    title,
+    description,
+    authors: [{name, url}],
+    robots: {
+        index: true,
+        follow: true,
+    },
+    twitter: {
+        title,
+        description,
+        card: "summary",
+    },
+    openGraph: {
+        title,
+        description,
+        url,
+        type: "website",
+    },
+};
 
 export default function RootLayout({
     children,
